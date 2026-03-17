584076abd5312c910d069ef6b286c61e9ba5db67
mvsth <mvstry@users.noreply.github.com>
2026-03-17 17:22:21 +0100

    fix: correct BrowserRouter basename to /waifu-alter/

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter basename="/waifualter">
+      <BrowserRouter basename="/waifu-alter">
         <App />
       </BrowserRouter>
     </ThemeProvider>
