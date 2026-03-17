bb775daacd4acd28eb705047759d1a57dd1ff998
mvsth <mvstry@users.noreply.github.com>
2026-03-18 00:24:03 +0100

    fix: remove /waifu-alter basename from router

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter basename="/waifu-alter">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </ThemeProvider>
